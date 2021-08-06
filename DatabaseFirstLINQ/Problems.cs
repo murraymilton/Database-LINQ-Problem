06a3755092abc9fe8561731770a451b5f293e497
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-06 20:15:58 +0200

    added a LINQ query for returning all products total purchase amount of a customer account

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -140,6 +140,11 @@ namespace DatabaseFirstLINQ
             // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "oda@gmail.com" and returns the sum of all of the products prices.
             // HINT: End of query will be: .Select(sc => sc.Product.Price).Sum();
             // Then print the total of the shopping cart to the console.
+            var totalPrice = _context.ShoppingCarts.Include(entry => entry.Product).Include(entry => entry.User).Where(entry => entry.User.Email == "oda@gmail.com")
+            .Select(sc => sc.Product.Price).Sum();
+            {
+                Console.WriteLine(totalPrice);
+            }
 
         }
 
