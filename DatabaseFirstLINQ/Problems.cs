6c1616b6cc2a038ab4518368c9c114f705e5503b
Zachary <taylorzw96@gmail.com>
2021-08-06 12:51:43 -0500

    Created LINQ query that returns users who registered after 2016 and before 2018

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -20,7 +20,7 @@ namespace DatabaseFirstLINQ
             ProblemThree();
             ProblemFour();
             ProblemFive();
-            //ProblemSix();
+            ProblemSix();
             //ProblemSeven();
             //ProblemEight();
             //ProblemNine();
@@ -101,7 +101,11 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets all of the users who registered AFTER 2016 and BEFORE 2018
             // Then print each user's email and registration date to the console.
-
+            var users = _context.Users.Where(u => u.RegistrationDate > new DateTime(2016, 1, 1)  &&  u.RegistrationDate < new DateTime(2018, 1, 1));
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.Email + ": " + user.RegistrationDate);
+            }
         }
 
         // <><><><><><><><> R Actions (Read) with Foreign Keys <><><><><><><><><>
